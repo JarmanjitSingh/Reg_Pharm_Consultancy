4aa19dbd5ab4f3f997339f6c96ca80c4c7502bda
JarmanjitSingh <jarmanjits176@gmail.com>
2024-12-05 23:49:46 +0530

    Added the google analytics with website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,18 @@ import SixthSection from "@/sections/sixth-section";
 import ThirdSection from "@/sections/third-section";
 import { Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
 
 export default function Home() {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   useEffect(() => {
     setIsSmallScreen(window.innerWidth < 768);
+    // Initialize Google Analytics
+    ReactGA.initialize("G-M48MQK9S55"); // Replace with your Measurement ID
+
+    // Track the initial page view
+    ReactGA.send("pageview");
   }, []);
 
   if (isSmallScreen) {
